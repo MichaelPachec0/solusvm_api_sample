9cc50b83d141f8e01e04baf215e4384e8f312f83
Michael Pacheco <michaelpacheco@protonmail.com>
2021-03-17 03:17:00 -0700

    Refactoring and enhancements * broke up code into main and api caller functions * Added type hints to the api caller function
    Signed-off-by: Michael Pacheco <michaelpacheco@protonmail.com>

--- src/main.py
+++ src/main.py
@@ -18,30 +18,9 @@ import requests
 #                           <bw>total,used,free,percent_used</bw>
 
 
-settings = open("../settings.txt", "r")
-# if you want to continuously run in the terminal, change False in settings.txt to True
-loop = True if settings.readline().strip('\n') == "True" else False
-# amount of time to sleep if looping, in minutes. change in settings.txt
-minutes = int(settings.readline().strip('\n'))
-settings.close()
-
-token = open("../tokens.txt", "r")
-
-post_flags = {
-    "key": token.readline().strip('\n'),
-    "hash": token.readline().strip('\n'),
-    "action": "info",
-    "bw": "true",
-    "hdd": "true",
-    # false for now, only outputs 0's in Racknerd's case
-    "mem": "false"
-}
-token.close()
 types = ["hdd", "bw", "mem"]
 
 oTypes = ["hostname", "ipaddress"]
-
-url = "https://nerdvm.racknerd.com/api/client/command.php"
 
 # breaks up tags
 oPat = re.compile(r'(<(?P<type>\w*?)>.*?</\w*?>)')
@@ -51,11 +30,11 @@ iPat = re.compile(r'<.*?>(?P<total>\d*?),(?P<used>\d*?),(?P<free>\d*?),(?P<per>\
 # assumes that you only have a single hostname and ip address,
 sPat = re.compile(r'<.*?>(?P<string>.*?)</.*?>')
 
-while True:
-    response = requests.request("POST", url, data=post_flags)
+
+def api_response(flags: dict, api_url: str) -> dict:
     resp = {}
 
-    for i in oPat.finditer(response.text):
+    for i in oPat.finditer(requests.request("POST", api_url, data=flags).text):
         if i.groups()[1] in types:
             s = iPat.match(i.group()).groupdict()
             lKey = list(s.keys())
@@ -83,19 +62,49 @@ while True:
             o = sPat.match(i.group()).groups()[0]
             resp[i.groups()[1]] = o
 
-    print(f"{'-' * 61}\n"
-          f"\t\t\tVPS status at {time.ctime()}\n"
-          f"{'-' * 61}\n"
-          f"Hostname: {resp['hostname']:<35}IP:{resp['ipaddress']:}\n")
-    for pType in types:
-        if post_flags[pType] == "true":
-            print(f"{pType:<4}:", end="\t")
-            for k in ['total', 'used', 'free']:
-                print(f"{k:>3}: {'%.2f' % resp[pType][k + '_display']:>5}{resp[pType][k + '_mag']}", end="\t")
-            print(f"{'%.2f' % resp[pType]['per']}%")
-    print(f"{'-' * 61}")
+    return resp
 
-    if not loop:
-        break
 
-    time.sleep(minutes * 60)
+if __name__ == '__main__':
+
+    url = "https://nerdvm.racknerd.com/api/client/command.php"
+
+    settings = open("../settings.txt", "r")
+    # if you want to continuously run in the terminal, change False in settings.txt to True
+    loop = True if settings.readline().strip('\n') == "True" else False
+    # amount of time to sleep if looping, in minutes. change in settings.txt
+    minutes = int(settings.readline().strip('\n'))
+    settings.close()
+
+    token = open("../tokens.txt", "r")
+
+    post_flags = {
+        "key": token.readline().strip('\n'),
+        "hash": token.readline().strip('\n'),
+        "action": "info",
+        "bw": "true",
+        "hdd": "true",
+        # false for now, only outputs 0's in Racknerd's case
+        "mem": "false"
+    }
+    token.close()
+
+    while True:
+        response = api_response(post_flags, url)
+        print(f"{'-' * 61}\n"
+              f"\t\t\tVPS status at {time.ctime()}\n"
+              f"{'-' * 61}\n"
+              f"Hostname: {response['hostname']:<35}IP:{response['ipaddress']:}\n")
+        for pType in types:
+            if post_flags[pType] == "true":
+                print(f"{pType:<4}:", end="\t")
+                for k in ['total', 'used', 'free']:
+                    print(f"{k:>3}: {'%.2f' % response[pType][k + '_display']:>5}{response[pType][k + '_mag']}",
+                          end="\t")
+                print(f"{'%.2f' % response[pType]['per']}%")
+        print(f"{'-' * 61}")
+
+        if not loop:
+            break
+
+        time.sleep(minutes * 60)
